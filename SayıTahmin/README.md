bcb322ae4ff3818a02cb7f6073cbe2605aa29aed
MaxiRaenn <94918455+MaxiRaenn@users.noreply.github.com>
2021-11-23 18:40:40 +0300

    Add files via upload

---
+++ SayıTahmin/README.md
@@ -0,0 +1,9 @@
+title = number guessing game
+author = Barkın Ege Dilek
+
+
+ENGLISH: program determines a number between 0 and 250
+
+TÜRKÇE: program 0 ile 250 arasında bir sayı belirler
+
+
---
+++ SayıTahmin/main.py
@@ -0,0 +1,18 @@
+from random import randint
+
+sayi1 = randint(0,250)
+sayac = 0
+
+while True:
+    sayac += 1
+    sayi = int(input("0 ile 250 arasında değer girin: "))
+    if sayi < sayi1:
+        print("Daha Yüksek Bir Sayı Giriniz")
+        continue
+    elif sayi > sayi1:
+        print("Daha Düşük Bir Sayı Giriniz")
+        continue
+    else:
+        print("Rastele seçilen sayı {0}!".format(rand))
+        print("Tahmin sayınız {0}".format(sayac))
+        input("")
